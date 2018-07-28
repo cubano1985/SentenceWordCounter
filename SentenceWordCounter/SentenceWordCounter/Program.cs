1e5376f234686844f779025eb4ea886f3af98441
Cubano <pawel.solowczuk@gmail.com>
2018-07-29 01:31:12 +0200

    Initial empty solution

---
+++ SentenceWordCounter/SentenceWordCounter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceWordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
